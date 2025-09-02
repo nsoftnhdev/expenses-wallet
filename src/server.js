f52fd9a88b8e4123a313e3b6c3f247bdd3eaefc3
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-09-02 12:40:03 +0800

    initial commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js";
+import ratelimiter from "./middleware/rateLimiter.js";
+import transactionsRoute from "./routes/transactionsRoute.js";
+
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(ratelimiter);
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+
+app.use("/api/transactions", transactionsRoute);
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
+  });
+});
