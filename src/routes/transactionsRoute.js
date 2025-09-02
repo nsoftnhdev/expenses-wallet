7dedeed77946c5e351c3f4c8166e4d867cd453b3
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-09-02 13:54:43 +0800

    third commit

--- src/routes/transactionsRoute.js
+++ src/routes/transactionsRoute.js
@@ -3,9 +3,9 @@ import { createTransaction, deleteTransaction, getSummaryByUserId, getTransactio
 
 const router = express.Router();
 
+router.get("/summary/:userId", getSummaryByUserId);
 router.get("/:userId", getTransactionsByUserId);
 router.post("/", createTransaction);
 router.delete("/:id", deleteTransaction);
-router.get("/summary/:userId", getSummaryByUserId);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { initDB } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
-
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(ratelimiter);
 app.use(express.json());
 
